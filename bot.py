9c30e32cd4789bd3f7449d4ae39b775fd5788c7c
Afrsto <afratosa@gmail.com>
2026-07-15 20:29:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,9 +15,6 @@ from zoneinfo import ZoneInfo
 from urllib.parse import urlparse
 import io
 
-from dotenv import load_dotenv
-load_dotenv()
-
 import discord
 from discord import app_commands
 from discord.ext import commands
